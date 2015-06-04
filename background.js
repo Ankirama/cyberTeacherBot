2c406691725eff147655740e4589a959c4e5edac
Ankirama <Ankirama@users.noreply.github.com>
2015-06-04 12:07:05 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete' && tab.active) {
     // do your things
-    chrome.tabs.executeScript(null, {code:"var test = window.frames['mainCyber'].frames['exo']; test.document.getElementsByName('response')[0].value = test.document.getElementsByName('result')[0].value; test.document.getElementById('s').submit();"});
+    chrome.tabs.executeScript(null, {code:"var test = window.frames['mainCyber'].frames['exo']; test.document.getElementsByName('response')[0].setAttribute('value', test.document.getElementsByName('result')[0].value); test.document.getElementById('s').submit();"});
   }
 })
